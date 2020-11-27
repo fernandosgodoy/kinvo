7aa8aac2636c07fad55b99604e2d8755e043a89d
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-11-27 08:58:08 -0300

    incluida a BaseEntity

---
+++ Kinvo.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinvo.Domain.Entities
+{
+    public class BaseEntity
+    {
+
+        public BaseEntity() { }
+
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtu { get; set; }
+    }
+}
