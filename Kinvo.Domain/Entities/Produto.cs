a9ec9aed1e7def3e5a8097348818e91f3e56351b
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-11-30 16:05:17 -0300

    Incluindo os modelos do sistema

---
+++ Kinvo.Domain/Entities/Produto.cs
@@ -0,0 +1,18 @@
+﻿using Kinvo.Domain.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinvo.Domain.Entities
+{
+    public class Produto
+        : BaseEntity
+    {
+
+        public string Nome { get; set; }
+        public CategoriaAtivo CategoriaAtivo { get; set; }
+
+    }
+}
---
+++ Kinvo.Domain/Enumerators/CategoriaAtivo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinvo.Domain.Enumerators
+{
+    public enum CategoriaAtivo
+    {
+
+        RendaFixa = 1,
+        RendaVariavel = 2,
+        TesouroDireto = 3,
+
+    }
+}
