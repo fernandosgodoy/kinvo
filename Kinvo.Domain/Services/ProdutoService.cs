f8cd8bafa77b206fd2bda1dbca2f103ef4975963
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-08 10:04:45 -0300

    Criado o servico para o Produto

---
+++ Kinvo.Domain/Services/ProdutoService.cs
@@ -0,0 +1,15 @@
+﻿using Kinvo.Domain.Entities;
+using Kinvo.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinvo.Domain.Services
+{
+    public class ProdutoService
+        : ServiceBase<Produto>, IProdutoService
+    {
+    }
+}
---
+++ Kinvo.Domain/Services/ServiceBase.cs
@@ -0,0 +1,48 @@
+﻿using Kinvo.Domain.Entities;
+using Kinvo.Domain.Interfaces.Repositories;
+using Kinvo.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinvo.Domain.Services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity>
+        where TEntity : BaseEntity
+    {
+
+        protected readonly IRepository<TEntity> _repository;
+
+        public TEntity Find(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public int InsertItem(TEntity entity)
+        {
+            return _repository.Add(entity);
+        }
+
+        public IEnumerable<TEntity> ListAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void RemoveItem(int id)
+        {
+            _repository.RemoveById(id);
+        }
+
+        public void RemoveItem(TEntity entity)
+        {
+            _repository.Remove(entity);
+        }
+
+        public void UpdateItem(TEntity entity)
+        {
+            _repository.Update(entity);
+        }
+    }
+}
