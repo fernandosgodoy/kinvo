ae29f1b4f68e572cb8170b94f8fc296e70ec3a86
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-08 10:47:49 -0300

    incluido o Context

---
+++ Kinvo.Infra.Data/Context/ProdutoContext.cs
@@ -0,0 +1,79 @@
+﻿using Kinvo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Kinvo.Infra.Data.Context
+{
+    public class ProdutoContext
+        : DbContext
+    {
+        public ProdutoContext(DbContextOptions<ProdutoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        // DbSets
+        public DbSet<Produto> Produtos { get; set; }
+
+        public IDbContextTransaction Transaction { get; private set; }
+
+        public IDbContextTransaction InitTransaction()
+        {
+            if (Transaction == null) Transaction = this.Database.BeginTransaction();
+            return Transaction;
+        }
+
+        private void RollBack()
+        {
+            if (Transaction != null)
+                Transaction.Rollback();
+        }
+
+        private void Commit()
+        {
+            if (Transaction != null)
+            {
+                Transaction.Commit();
+                Transaction.Dispose();
+                Transaction = null;
+            }
+        }
+
+        public void SendChanges()
+        {
+            Save();
+            Commit();
+        }
+
+        private void Save()
+        {
+            try
+            {
+                ChangeTracker.DetectChanges();
+                SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                RollBack();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.ApplyConfiguration(new ProdutoMap());
+
+        }
+    }
+}
