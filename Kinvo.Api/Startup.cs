850469047c5c0fe3f7d98f64bde151cf2e78655a
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-10 11:52:14 -0300

    mudança nas referências para executar os Migration

--- Kinvo.Api/Startup.cs
+++ Kinvo.Api/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Kinvo.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace Kinvo.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ProdutoContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
