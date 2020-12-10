f679632056a75e0d00310283f05c2d7aefba8f75
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-10 11:24:08 -0300

    Criado o repositório de produtos

---
+++ Kinvo.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Kinvo.Domain.Entities;
+using Kinvo.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kinvo.Infra.Data.Repositories
+{
+    public class ProdutoRepository<T>
+        : RepositoryBase<T>
+        where T : BaseEntity
+    {
+        public ProdutoRepository(ProdutoContext contexto) : base(contexto)
+        {
+        }
+    }
+}
