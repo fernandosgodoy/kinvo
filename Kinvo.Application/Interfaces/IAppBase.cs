a0835337a730da1ee084a018d5621037b91e66fb
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-12 17:48:57 -0300

    Interface da Aplicação

---
+++ Kinvo.Application/Interfaces/IAppBase.cs
@@ -0,0 +1,20 @@
+﻿using Kinvo.Application.DTO;
+using Kinvo.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kinvo.Application.Interfaces
+{
+    public interface IAppBase<TEntidade, TEntidadeDTO>
+        where TEntidade : BaseEntity
+        where TEntidadeDTO : DTOBase
+    {
+        int Incluir(TEntidadeDTO entidade);
+        void Excluir(int id);
+        void Excluir(TEntidadeDTO entidade);
+        void Alterar(TEntidadeDTO entidade);
+        TEntidadeDTO SelecionarPorId(int id);
+        IEnumerable<TEntidadeDTO> SelecionarTodos();
+    }
+}
