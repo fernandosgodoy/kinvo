62ad41af807a231b146e343ce2a2e09418c904e3
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-12 17:55:09 -0300

    Chamada do ProdutoApp

---
+++ Kinvo.Application/Services/ProdutoApp.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Kinvo.Application.DTO;
+using Kinvo.Application.Interfaces;
+using Kinvo.Domain.Entities;
+using Kinvo.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kinvo.Application.Services
+{
+    public class ProdutoApp
+        : ServicoAppBase<Produto, ProdutoDTO>, IProdutoApp
+    {
+
+        public ProdutoApp(IMapper mapper, IProdutoService service)
+            : base(mapper, service)
+        { }
+
+    }
+}
