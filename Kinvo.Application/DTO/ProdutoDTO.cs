c07b32bd793d377e137a8070b95ea57fc31e1725
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-12 17:46:47 -0300

    Criada a camada de aplicação

---
+++ Kinvo.Application/DTO/ProdutoDTO.cs
@@ -0,0 +1,23 @@
+﻿using Kinvo.Domain.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kinvo.Application.DTO
+{
+    public class ProdutoDTO
+        : DTOBase
+    {
+
+        public string Nome { get; set; }
+        public CategoriaProduto CategoriaProduto { get; set; }
+        public string AtivoSigla { get; set; }
+        public int? Qtde { get; set; }
+        public string Instituicao { get; set; }
+        public decimal PrecoCompra { get; set; }
+        public DateTime DataInicio { get; set; }
+        public decimal? TaxaCorretagem { get; set; }
+        public decimal? TaxaAnual { get; set; }
+
+    }
+}
---
+++ Kinvo.Application/MappingEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoMapper;
+using Kinvo.Application.DTO;
+using Kinvo.Domain.Entities;
+
+namespace Kinvo.Application
+{
+    public class MappingEntities
+        : Profile
+    {
+        public MappingEntities()
+        {
+            CreateMap<Produto, ProdutoDTO>();
+            CreateMap<ProdutoDTO, Produto>();
+        }
+
+        public MappingEntities(string profileName)
+            : base(profileName)
+        {
+        }
+    }
+}
