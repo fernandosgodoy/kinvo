495322e94cb9590b8fe09b34c71aa1f330215aef
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-12 17:40:17 -0300

    Incluida a camada de IoC

---
+++ Kinvo.Infra.IoC/InjectorDependencies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Kinvo.Infra.IoC
+{
+    public class InjectorDependencies
+    {
+        public static void Register(IServiceCollection serviceCollection)
+        {
+            //Aplicação
+            //serviceCollection.AddScoped(typeof(IAppBase<,>), typeof(ServicoAppBase<,>));
+            //serviceCollection.AddScoped<ICondominioApp, CondominioApp>();
+
+            //Dominio
+            //serviceCollection.AddScoped(typeof(IServiceBase<>), typeof(ServiceBase<>));
+            //serviceCollection.AddScoped<ICondominioService, CondominioService>();
+
+            //Repositorio
+            //serviceCollection.AddScoped(typeof(IRepository<>), typeof(RepositoryBase<>));
+            //serviceCollection.AddScoped<ICondominioRepository, CondominioRepository>();
+
+        }
+    }
+}
