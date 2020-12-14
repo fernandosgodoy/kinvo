f3cda4d2fb446da0bd07946997494d0b94ff5002
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-13 21:54:37 -0300

    Adicionado o arquivo de Migration novamente

---
+++ Kinvo.Infra.Data/Migrations/20201214005339_kinvo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kinvo.Infra.Data.Migrations
+{
+    public partial class kinvo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DataCriacao = table.Column<DateTime>(nullable: false),
+                    DataAtu = table.Column<DateTime>(nullable: true),
+                    Nome = table.Column<string>(maxLength: 50, nullable: false),
+                    CategoriaProduto = table.Column<int>(nullable: false),
+                    AtivoSigla = table.Column<string>(maxLength: 8, nullable: true),
+                    Qtde = table.Column<int>(nullable: false),
+                    Instituicao = table.Column<string>(maxLength: 30, nullable: false),
+                    PrecoCompra = table.Column<decimal>(nullable: false),
+                    DataInicio = table.Column<DateTime>(nullable: false),
+                    TaxaCorretagem = table.Column<decimal>(nullable: true),
+                    TaxaAnual = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produtos");
+        }
+    }
+}
