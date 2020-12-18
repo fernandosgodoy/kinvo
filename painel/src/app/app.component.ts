55a2dc9599140853a004739c8ec598c7867895ca
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-18 19:13:15 -0300

    Criando lógica do login

--- painel/src/app/app.component.ts
+++ painel/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,24 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   title = 'painel';
-  login: string = "";
-  senha: string = "";
+  isSignedIn = false;
+
+  constructor(public firebaseService: FirebaseService){ }
+ 
+  ngOnInit() {
+    if (localStorage.getItem('user') !== null)
+      this.isSignedIn = true;
+    else
+      this.isSignedIn = false;
+  }
+
+  async entrar(email: string, senha: string) {
+    console.log(email, senha);
+    await this.firebaseService.signin(email, senha)
+      .then(r => {
+        if (this.firebaseService.isLoggedIn)
+          this.isSignedIn = true;
+      });
+  }
 
 }
