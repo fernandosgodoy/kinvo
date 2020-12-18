3eab1ec5346db7ac1d0804b9cd44e1436b970d59
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-18 19:45:59 -0300

    Tela de Login

--- painel/src/app/app.module.ts
+++ painel/src/app/app.module.ts
@@ -4,8 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {environment} from '../environments/environment';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -15,9 +17,13 @@ import {environment} from '../environments/environment';
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+    AngularFireModule,
+    AngularFireAuth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
